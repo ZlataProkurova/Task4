5f36fae8eae64966137b9680920e6611e3bda0d2
ZlataProkurova <zlata.dmitrieva.2014@mail.ru>
2021-10-01 11:31:12 +0300

    first commit

--- dz2.py
+++ dz2.py
@@ -1,8 +1,8 @@
-cathet1 = float(input ("Введите длину первого катета: "))
-cathet2 = float(input ("Введите длину второго катета: "))
-hypotenuse = (cathet1 ** 2 + cathet2 ** 2) ** 0.5
-perimeter = (cathet1 + cathet2 + hypotenuse)
-square = (cathet1 * cathet2) / 2
+c1 = float(input ("Введите длину первого катета: "))
+c2 = float(input ("Введите длину второго катета: "))
+hypotenuse = (c1 ** 2 + c2 ** 2) ** 0.5
+perimeter = (c1 + c2 + hypotenuse)
+square = (c1 * c2) / 2
 print ("Площадь прямоугольного треугольника: " + str(square))
 print("Периметр прямоугольного треугольника: " + str(perimeter))
 input()
