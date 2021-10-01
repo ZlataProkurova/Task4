824f5ab824809e94028cbc83677cbb0354a6ccaa
ZlataProkurova <zlata.dmitrieva.2014@mail.ru>
2021-10-01 11:39:38 +0300

    first commit

---
+++ 4.py
@@ -0,0 +1,10 @@
+years = int(input("Количество лет: "))
+exhibits = years*365*96
+print("Количество экспонатов: "+ str(exhibits))
+exhibits2 = int(input("Количество просмотренных экспонатов: "))
+minutes = exhibits2*5
+hours = minutes/60
+days = hours/8
+years2 = days/365
+print("Количество лет: "+ str(years2))
+input()
