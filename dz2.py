5218991ebbe06c1bfe172d22f9098ceba27f7431
ZlataProkurova <91047338+ZlataProkurova@users.noreply.github.com>
2021-10-01 11:45:54 +0300

    Delete dz2.py

--- dz2.py
+++
@@ -1,8 +0,0 @@
-c1 = float(input ("Введите длину первого катета: "))
-c2 = float(input ("Введите длину второго катета: "))
-hypotenuse = (c1 ** 2 + c2 ** 2) ** 0.5
-perimeter = (c1 + c2 + hypotenuse)
-square = (c1 * c2) / 2
-print ("Площадь прямоугольного треугольника: " + str(square))
-print("Периметр прямоугольного треугольника: " + str(perimeter))
-input()
